88e60a3021fb9936fc100f309d0f4202ec7d8566
Nicholas Wakeford <nicwakeford@gmail.com>
2014-09-18 16:58:49 -0500

    changed location of test

---
+++ Git Test/NHDWTest.h
@@ -0,0 +1,13 @@
+//
+//  NHDWTest.h
+//  Git Test
+//
+//  Created by Nicholas Wakeford on 9/18/14.
+//  Copyright (c) 2014 Nicholas Henry Dean Wakeford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NHDWTest : NSObject
+
+@end
---
+++ Git Test/NHDWTest.m
@@ -0,0 +1,13 @@
+//
+//  NHDWTest.m
+//  Git Test
+//
+//  Created by Nicholas Wakeford on 9/18/14.
+//  Copyright (c) 2014 Nicholas Henry Dean Wakeford. All rights reserved.
+//
+
+#import "NHDWTest.h"
+
+@implementation NHDWTest
+
+@end
